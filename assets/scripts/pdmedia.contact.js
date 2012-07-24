e8e01b6f6a079902570a64426326de00034c152a
Sean Winter <sean@contactapp.com>
2012-07-24 15:36:11 -0700

    minor updates

--- assets/scripts/pdmedia.contact.js
+++ assets/scripts/pdmedia.contact.js
@@ -10,13 +10,19 @@ $(document).ready(function()
 
 function parseXml(xml)
 {
+	alert('Parse function firing now.');
 	$(xml).find('entry').each(function()
 	{ 
 		//populate variables with employee information
 		var fname = $(this).find('f-name').text(),
 		lname = $(this).find('l-name').text();
 		
-		//build markup
-		$('#contacts #content').append(fname + ' ' + lname + '<br/>');
+		//build markup for employee list
+		$('#contacts #content').append('<li><a href="#">' + fname + ' ' + lname + '</a></li>');
 	});
+}
+
+function empDetail()
+{
+	
 }
