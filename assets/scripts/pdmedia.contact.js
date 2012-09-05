78ec8bd4add08494c11f158aab39cf8278874127
Sean Winter <sean@contactapp.com>
2012-09-05 16:13:12 -0700

    switched to json for data

--- assets/scripts/pdmedia.contact.js
+++ assets/scripts/pdmedia.contact.js
@@ -1,10 +1,47 @@
-$.ajax({
-    type: "GET",
-    url: "assets/data/contacts.xml",
-    dataType: "xml",
-    success: parseContacts,
-		complete: function(){}
-});
+$(document).ready(function(){
+	if(!window.localStorage.getItem('test')){
+		window.localStorage.setItem('test','we aint found shit');
+	}else{
+		alert('found variable');
+		//window.localStorage.removeItem('test');
+		//window.localStorage.setItem('test', 'replaced shit');
+	}	
+	console.log(window.localStorage.getItem('test'));
+	
+	$.getJSON('assets/data/contacts.json', function(data){
+		$.each(data.contact.contacts, function(i,item)
+		{	
+			//json nodes: id, f_name, l_name, phone_1, phone_2, dept, title, pic, email	
+			//build employee list with specific info in data attributes
+			$('#contacts #content').append('<li><a href="#details" data-id="' + item.id + '" data-first="' + item.f_name + 
+			'" data-last="' + item.l_name + '" data-phone1="' + item.phone_1 + '" data-phone2="' + item.phone_2 + '"' +
+			' data-title="' + item.title + '" data-dept="' + item.dept + '" data-email="' + item.email + '" data-pic="' + item.pic + '">' + 
+			'<img src="assets/images/portraits/' + item.pic + '" /><h3>' + item.f_name + ' ' + item.l_name + '</h3><p>Phone: ' + item.phone_1 + '</p></a></li>');
+		});
+		$('#content li a').each(function(){
+			$(this).click(function(){
+	
+				var html = '', ul_list = '';
+				
+				//populate variable HTML with data attributes and markup			
+				html = '<img class="img-portrait" src="assets/images/portraits/' + $(this).attr('data-pic') + '">';
+				html += '<h3>' + $(this).attr('data-first') + ' ' + $(this).attr('data-last') + '</h3>';
+				html += '<div class="clear"></div>';
+				
+				ul_list = '<li>Title: ' + $(this).attr('data-title') + '</li>';
+				ul_list += '<li>Department: ' + $(this).attr('data-dept') + '</li>';
+				ul_list += '<li>Phone 1: ' + $(this).attr('data-phone1') + '</li>';
+				ul_list += '<li>Phone 2: ' + $(this).attr('data-phone2') + '</li>';
+				ul_list += '<li>Email: ' + $(this).attr('data-email') + '</li>';					
+				
+				//build details html
+				$('#details #detail-content #emp-img').html(html);
+				$('ul#emp-details').html(ul_list);
+				
+				});// end click			
+		});// end .each a		
+	});// end .getJSON
+});// end docready
 	
 $("#details").live(
 	"pagebeforeshow",
@@ -12,51 +49,5 @@ $("#details").live(
 			//refresh the ul to show styles appropriately
 		$('ul#emp-details').listview('refresh', true);
 	}
-	);
+);
 	
-function parseContacts(xml)
-{
-	$(xml).find('entry').each(function()
-	{ 
-		//populate variables with employee information from XML
-		var fname = $(this).find('f-name').text(),
-		lname = $(this).find('l-name').text(),
-		phone_1 = $(this).find('phone-1').text(),
-		phone_2 = $(this).find('phone-2').text(),
-		title = $(this).find('title').text(),
-		dept = $(this).find('dept').text(),
-		email = $(this).find('email').text(),
-		pic = $(this).find('pic').text(),
-		empID = $(this).attr('id');
-		
-		//build employee list with specific info in data attributes
-		$('#contacts #content').append('<li><a href="#details" data-id="' + empID + '" data-first="' + fname + 
-		'" data-last="' + lname + '" data-phone1="' + phone_1 + '" data-phone2="' + phone_2 + '" data-phone2="' + phone_2 + '"' +
-		' data-title="' + title + '" data-dept="' + dept + '" data-email="' + email + '" data-pic="' + pic + '">' + 
-		'<img src="assets/images/portraits/' + pic + '" /><h3>' + fname + ' ' + lname + '</h3><p>Phone: ' + phone_1 + '</p></a></li>');
-	});
-	$('#content li a').each(function(){
-		$(this).click(function(){
-
-			var html = '', ul_list = '';
-			
-			//populate variable HTML with data attributes and markup			
-			html = '<img class="img-portrait" src="assets/images/portraits/' + $(this).attr('data-pic') + '">';
-			html += '<h3>' + $(this).attr('data-first') + ' ' + $(this).attr('data-last') + '</h3>';
-			html += '<div class="clear"></div>';
-			
-			ul_list = '<li>Title: ' + $(this).attr('data-title') + '</li>';
-			ul_list += '<li>Department: ' + $(this).attr('data-dept') + '</li>';
-			ul_list += '<li>Phone 1: ' + $(this).attr('data-phone1') + '</li>';
-			ul_list += '<li>Phone 2: ' + $(this).attr('data-phone2') + '</li>';
-			ul_list += '<li>Email: ' + $(this).attr('data-email') + '</li>';					
-			
-			//build details html
-			$('#details #detail-content #emp-img').html(html);
-			$('ul#emp-details').html(ul_list);
-			
-			});
-			
-	});
-
-}
